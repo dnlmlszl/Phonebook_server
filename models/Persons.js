a7fca3f4b33497ac5c6bca6f44ca1bc11177c213
daniel.mlaszlo@yahoo.com <daniel.mlaszlo@yahoo.com>
2023-10-08 16:23:17 +0200

    Error handling, validators

--- models/Persons.js
+++ models/Persons.js
@@ -4,11 +4,22 @@ const PersonSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      minLength: [3, 'Name must be at least 3 characters long.'],
+      required: [true, 'Name is required.'],
+      unique: true,
     },
     number: {
       type: String,
-      required: true,
+      minlength: [8, 'Number must be at least 8 characters long.'],
+      validate: {
+        validator: function (v) {
+          // Regular expression for the phone number format
+          const phoneRegex = /^\d{2,3}-\d{7,}$/;
+          return phoneRegex.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+      required: [true, 'Number is required.'],
     },
   },
   { timestamps: true }
